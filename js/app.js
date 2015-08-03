0fbfbf48b556525aa21c4350df8bc50a31fdf50d
honger <honger05@126.com>
2015-08-03 17:32:28 +0800

    f

---
+++ js/app.js
@@ -0,0 +1,49 @@
+;(function($, owner) {
+	
+	var URL_LOGIN = 'http://192.168.10.215/cl-restapi/login.do';
+	
+	owner.login = function(loginInfo, callback) {
+		callback = callback || $.noop;
+		if (loginInfo.account.length <= 0) {
+			return callback('请输入账号');
+		}
+		if (loginInfo.password.length <= 0) {
+			return callback('请输入密码');
+		}
+//		$.post(URL_LOGIN, loginInfo, function(data) {
+//			var state = data.sessionUser;
+//			state.token = data.token;
+//			state.status = data.status;
+//			owner.setState(state);
+//			callback();
+//		}, 'json');
+		owner.setState(loginInfo);
+		callback();
+	}
+	
+	$.ready(function() {
+		$('body').on('click', 'a', function(e) {
+			e.preventDefault();
+		})
+	});
+	
+	owner.setSettings = function(settings) {
+		settings = settings ||{};
+		localStorage.setItem('$settings', JSON.stringify(settings));
+	}
+	
+	owner.getSettings = function() {
+		var settingsText = localStorage.getItem('$setting') || '{}';
+		return JSON.parse(settingsText);
+	}
+	
+	owner.setState = function(state) {
+		state = state || {};
+		localStorage.setItem('$state', JSON.stringify(state));
+	}
+	
+	owner.getState = function() {
+		var stateText = localStorage.getItem('$state') || '{}';
+		return JSON.parse(stateText);
+	}
+})(mui, window.app = {});
---
+++ js/goods.js
@@ -0,0 +1,9 @@
+(function($, cl, websql) {
+	
+	var URL_SUMMAY = 'http://192.168.10.215/cl-restapi/stock/query/summary.do';
+	
+	cl.getSummary = function(datainfo, onSuccess) {
+		$.post(URL_SUMMAY, datainfo, onSuccess, 'json');
+	}
+	
+})(mui, window.cl = {}, html5sql);
